1d7561e74ac98210b168a0502db5f29248fce87c
pascalbakker <pascalsbakker@gmail.com>
2025-04-09 17:53:15 -0500

    feat: added autostart

--- config.h
+++ config.h
@@ -37,6 +37,10 @@ static const char *colors[][3] = {
     [SchemeSel] = {col_gray4, col_cyan, col_ivyred},
 };
 
+static const char *const autostart[] = {
+    "st", NULL, NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
