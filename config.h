5e6a25c7261db5677e50bf47316b81d0fde66397
pascalbakker <pascalsbakker@gmail.com>
2025-04-09 18:28:51 -0500

    chore: use llvm

--- config.h
+++ config.h
@@ -5,25 +5,23 @@
 
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY, TAG)                                                      \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
-      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
-      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
-      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+    {MODKEY, KEY, view, {.ui = 1 << TAG}},                                     \
+        {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},             \
+        {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                      \
+        {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                             \
-  {                                                                            \
-    .v = (const char *[]) {                                                    \
-      "/bin/sh", "-c", cmd, NULL                                               \
-    }                                                                          \
-  }
+    {                                                                          \
+        .v = (const char*[]) { "/bin/sh", "-c", cmd, NULL }                    \
+    }
 
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
+static const char* fonts[] = {"monospace:size=10"};
 static const char dmenufont[] = "monospace:size=10";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
@@ -31,18 +29,18 @@ static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#005577";
 static const char col_ivyred[] = "#770000";
-static const char *colors[][3] = {
+static const char* colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
     [SchemeSel] = {col_gray4, col_cyan, col_ivyred},
 };
 
-static const char *const autostart[] = {
+static const char* const autostart[] = {
     "st", NULL, NULL /* terminate */
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char* tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -64,34 +62,21 @@ static const int lockfullscreen =
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"[Fib]", spiral},
-    {"[Tile]", tile},  /* first entry is default */
+    {"[Fib]", spiral},   {"[Tile]", tile}, /* first entry is default */
     {"[Float]", NULL}, /* no layout function means floating behavior */
-    {"[Full]", monocle},
-    {"[Dwindle]", dwindle},
+    {"[Full]", monocle}, {"[Dwindle]", dwindle},
 };
 
 /* key definitions */
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"dmenu_run",
-                                 "-m",
-                                 dmenumon,
-                                 "-fn",
-                                 dmenufont,
-                                 "-nb",
-                                 col_gray1,
-                                 "-nf",
-                                 col_gray3,
-                                 "-sb",
-                                 col_cyan,
-                                 "-sf",
-                                 col_gray4,
-                                 NULL};
+static const char* dmenucmd[] = {
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 
 /* PROGRAMS */
-static const char *termcmd[] = {"st", NULL};
+static const char* termcmd[] = {"st", NULL};
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
